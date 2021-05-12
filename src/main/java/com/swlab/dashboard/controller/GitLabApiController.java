762065b27ca578cebfc14901126bec7284c90366
jojojohhh <qwhtkdwp@gmail.com>
2021-05-12 23:00:03 +0900

    -

--- src/main/java/com/swlab/dashboard/controller/GitLabApiController.java
+++ src/main/java/com/swlab/dashboard/controller/GitLabApiController.java
@@ -7,19 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitLabApiController {
 
-    /*
-        GitLab 인증 요청을 보내는 API
-     */
-    @PostMapping("/auth/gitlab/authorize")
-    public void gitLabAuthorize() {
-        //https://gitlab.example.com/oauth/authorize?client_id=APP_ID&redirect_uri=REDIRECT_URI&response_type=code&state=STATE&scope=REQUESTED_SCOPES&code_challenge=CODE_CHALLENGE&code_challenge_method=S256
-    }
 
-    /*
-        GitLab 인증 결과 받는 API
-     */
-    @GetMapping("/auth/gitlab/callback")
-    public void gitLabAuthCallback() {
-        //https://example.com/oauth/redirect?code=1234567890&state=STATE
-    }
 }
