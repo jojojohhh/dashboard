0d6a94f6b2de8c6d4d0ad35b73d68b02787026d8
qwer8714 <qwhtkdwp@gmail.com>
2021-05-13 19:05:28 +0900

    add pages

--- src/main/java/com/swlab/dashboard/controller/HomeController.java
+++ src/main/java/com/swlab/dashboard/controller/HomeController.java
@@ -2,12 +2,44 @@ package com.swlab.dashboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/dashboard")
-    public String getHome() {
+    @GetMapping(value = {"", "/dashboard"})
+    public String getDashboard() {
         return "index";
+    }
+
+    @GetMapping("/calendar")
+    public String getCalendar() {
+        return "pages/calendar";
+    }
+
+    @GetMapping("/kanban")
+    public String getKanban() {
+        return "pages/kanban";
+    }
+
+    @GetMapping("/projects")
+    public String getProjects() {
+        return "pages/projects/projects";
+    }
+
+    @GetMapping("/project-add")
+    public String getProjectAdd() {
+        return "pages/projects/project-add";
+    }
+
+    @GetMapping("/project-detail")
+    public String getProjectDetail() {
+        return "pages/projects/project-detail";
+    }
+
+    @GetMapping("/contacts")
+    public String getContacts() {
+        return "pages/projects/contacts";
     }
 }
