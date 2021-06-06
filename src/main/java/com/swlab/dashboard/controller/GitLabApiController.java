00abb8491965bdfc092929e31a7a6a7abfcce45c
jojojohhh <qwhtkdwp@gmail.com>
2021-06-06 21:27:43 +0900

    GitLab API 라이브러리 의존성 추가

--- src/main/java/com/swlab/dashboard/controller/GitLabApiController.java
+++ src/main/java/com/swlab/dashboard/controller/GitLabApiController.java
@@ -1,11 +1,7 @@
 package com.swlab.dashboard.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitLabApiController {
-
-
 }
