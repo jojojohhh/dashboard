f89d61ec84c86375ee6518279b214d936ec44e44
jojojohhh <qwhtkdwp@gmail.com>
2021-07-28 22:34:02 +0900

    -

--- src/main/java/com/swlab/dashboard/repository/UserRoleRepository.java
+++ src/main/java/com/swlab/dashboard/repository/UserRoleRepository.java
@@ -2,6 +2,8 @@ package com.swlab.dashboard.repository;
 
 import com.swlab.dashboard.model.user.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 }
