30b9354f39e00b38460f74ef5cc982e07a0872f9
jojojohhh <qwhtkdwp@gmail.com>
2021-08-03 16:23:59 +0900

    404에러 NoHandlerFoundException를 ControllerAdivce를 사용하여 처리

--- src/main/java/com/swlab/dashboard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/swlab/dashboard/exception/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.swlab.dashboard.exception;
 
 import com.swlab.dashboard.utils.ApiResult;
 import org.gitlab4j.api.GitLabApiException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static com.swlab.dashboard.utils.ApiUtils.error;
 
@@ -12,12 +16,15 @@ import static com.swlab.dashboard.utils.ApiUtils.error;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GitLabApiException.class)
-    public ApiResult gitLabApiException(GitLabApiException e) {
+    public ApiResult gitLabApiException(GitLabApiException e, Model model) {
+        model.addAttribute("error", error(e.getMessage(), e.getHttpStatus()));
         return error(e.getMessage(), e.getHttpStatus());
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ApiResult notFoundException(NotFoundException e) {
-        return error(e.getMessage(), e.getStatus());
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String noHandlerFoundException(HttpServletRequest req, Exception e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        model.addAttribute("url", req.getRequestURL());
+        return "/err/404";
     }
 }
